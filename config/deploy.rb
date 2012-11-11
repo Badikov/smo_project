0fd5eb8c8713b852ee0a36e554be732f393cba3a
alex <badikov-a@yandex.ru>
2012-11-12 06:01:30 +0700

    base

--- config/deploy.rb
+++ config/deploy.rb
@@ -28,14 +28,15 @@
 # scp ~/.ssh/id_rsa.pub deployer@server:~/.ssh/authorized_keys
 #3333333333333333333333
 # RVM bootstrap
-$:.unshift(File.expand_path('./lib', ENV['rvm_path']))
-# require 'rvm/capistrano'
-set :rvm_ruby_string, 'ruby 1.9.3p286'
-set :rvm_type, :system
-set :bundle_cmd, 'source /home/alex/.rvm/scripts/rvm'
+# $:.unshift(File.expand_path('./lib', ENV['rvm_path']))
+require 'rvm/capistrano'
+set :rvm_ruby_string, 'ruby-1.9.3-p286'
+set :rvm_type, :user
+# set :bundle_cmd, 'source /home/deployer/.rvm/gems/ruby-1.9.3-p286@rails3tutorial/bin/bundle'
 
 # bundler bootstrap
 require 'bundler/capistrano'
+set :bundle_without, [:development, :test]
 # main details
 set :application, "smo_project"
 role :web, "217.116.153.106"
@@ -44,9 +45,9 @@ role :db,  "217.116.153.106", :primary => true
 # server details
 default_run_options[:pty] = true
 ssh_options[:forward_agent] = true
-set :deploy_to, "/home/alex/smo_project"
+set :deploy_to, "/home/deployer/webapps/#{application}"
 set :deploy_via, :remote_cache
-set :user, "alex"
+set :user, "deployer"
 set :password, "123108"
 set :use_sudo, false
 # repo details
@@ -56,36 +57,43 @@ set :repository, "git://github.com/Badikov/smo_project.git"
 set :branch, "master"
 # set :git_enable_submodules, 1
 # tasks
-    namespace :deploy do  
-      task :start, :roles => :app do  
-      end  
-        task :stop, :roles => :app do  
-	end  task :restart, :roles => :app do    
-	  run "touch #{release_path}/tmp/restart.txt"  
-	end  
-	task :after_update_code, :roles => :app do    
-	  run "rm -rf #{release_path}/public/.htaccess"  
-	  end
-    end  
-# namespace :deploy do
-#   task :start, :roles => :app do
-#     run "touch #{current_release}/tmp/restart.txt"
-#   end
-# 
-#   task :stop, :roles => :app do
-#     # Do nothing.
-#   end
-# 
-#   desc "Restart Application"
-#   task :restart, :roles => :app do
-#     run "touch #{current_release}/tmp/restart.txt"
-#   end
-# 
-#   desc "Symlink shared resources on each release - not used"
-#   task :symlink_shared, :roles => :app do
-# #     run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
-#   end
-# end
+namespace :deploy do 
+  namespace :assets do
+     task :precompile, :roles => :web, :except => { :no_release => true } do
+          from = source.next_revision(current_revision)
+          if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
+            run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
+          else
+            logger.info "Skipping asset pre-compilation because there were no asset changes"
+          end
+      end
+  end
+    
+  desc "Start server"
+  task :start, :roles => :app do 
+    run "#{try_sudo} touch #{File.join(release_path,'tmp','restart.txt')}"
+  end
+  
+  task :stop, :roles => :app do 
+    
+  end 
+  
+  desc "Restart server"
+  task :restart, :roles => :app do    
+    run "#{try_sudo} touch #{File.join(release_path,'tmp','restart.txt')}" 
+  end
+  
+  desc "Symlink shared configs and folders on each release."
+  task :symlink_shared, :roles => :app do
+    run "ln -nfs #{shared_path}/cached-copy/config/database.yml #{release_path}/config/database.yml"
+    #run "ln -nfs #{shared_path}/assets #{release_path}/public/assets"
+  end
+  
+  desc "Execute migrations"
+  task :migrate, :roles => :db do
+    run "bundle exec rake db:migrate"
+  end
+end  
 
 after 'deploy:update_code', 'deploy:symlink_shared'
 
