fe23ab0818060cdbfa649a95c04cd5410f2a23ff
alex <badikov-a@yandex.ru>
2012-11-16 16:14:38 +0700

    next2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,7 +16,7 @@ SmoProject::Application.configure do
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
-  config.assets.precompile += %w[print.css print_petition.css]
+  config.assets.precompile += %w[print.css print_petition.css print_polis.js print_petition.js]
 
   # Generate digests for assets URLs
   config.assets.digest = true
