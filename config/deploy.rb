d384e3c83a39ff13031c5c74f832a834ce8f92b1
alex <badikov-a@yandex.ru>
2012-11-16 15:47:31 +0700

    next

--- config/deploy.rb
+++ config/deploy.rb
@@ -66,12 +66,12 @@ namespace :deploy do
   namespace :assets do
     
     desc "Figure out modified assets."
-    task :determine_modified_assets, :roles => :app, :except => { :no_release => true } do
+    task :determine_modified_assets, :roles => assets_role, :except => { :no_release => true } do
       set :updated_assets, capture("find #{latest_release}/app/assets -type d -name .git -prune -o -mmin -#{max_asset_age} -type f -print", :except => { :no_release => true }).split
     end
     
     desc "Remove callback for asset precompiling unless assets were updated in most recent git commit."
-    task :conditionally_precompile, :roles => :app, :except => { :no_release => true } do
+    task :conditionally_precompile, :roles => assets_role, :except => { :no_release => true } do
       if(updated_assets.empty?)
         callback = callbacks[:after].find{|c| c.source == "deploy:assets:precompile" }
         callbacks[:after].delete(callback)
